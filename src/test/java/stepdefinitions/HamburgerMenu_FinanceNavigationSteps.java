1031e8484a5dd84afe4ab677d76a865dd544d287
tanerozc <tanerozcelik@gmail.com>
2026-01-04 16:38:31 +0000

    HamburgerMenu_FinanceNavigationSteps created and updated.

--- src/test/java/stepdefinitions/HamburgerMenu_FinanceNavigationSteps.java
+++ src/test/java/stepdefinitions/HamburgerMenu_FinanceNavigationSteps.java
@@ -1,4 +1,9 @@
 package stepdefinitions;
 
+import org.openqa.selenium.WebDriver;
+import utils.BaseDriver;
+
 public class HamburgerMenu_FinanceNavigationSteps {
+
+    WebDriver driver= BaseDriver.getDriver();
 }
